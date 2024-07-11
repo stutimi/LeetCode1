db22eb0c36d886dde1126b4b94f1c5509a8aac81
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2024-07-11 12:46:05 +0530

    Time: 9 ms (21.95%) | Memory: 14 MB (41.58%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<int>st;
+        for(auto log: logs){
+            if(log=="../"){
+                if(st.empty()) continue;
+                st.pop();
+            } else if(log=="./") continue;
+            else{
+                st.push(1);
+            }
+        }
+        return st.size();
+    }
+};
