2da623f1d6487589574debca2035463b9b7a8626
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2024-08-01 07:13:13 +0530

    Time: 8 ms (47.01%) | Memory: 17.7 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for (const auto& detail : details) {
+            int age = stoi(detail.substr(11, 2));
+            if (age > 60) {
+                count++;
+            }
+        }
+    return count;
+
+    }
+};
