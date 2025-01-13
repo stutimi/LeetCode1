c9ce80c47337441fffd28ac0046d8550cb3680d5
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2025-01-13 23:25:38 +0530

    Time: 9 ms (82.26%) | Memory: 32.1 MB (74.19%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> cnt(26);
+        for (const auto& x : s) {
+            ++cnt[x - 'a'];
+        }
+        return accumulate(cbegin(cnt), cend(cnt), 0, [](const auto& accu, const auto& x) {
+            return accu + (x ? 2 - x % 2 : 0);
+        });
+    }
+};
