ba5a9023b905dfca6d7b214cc86a36fef6d01490
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2025-01-18 08:11:14 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (65.02%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         int five = 0, ten = 0;
-        
         for (int bill : bills) {
-            if (bill == 5) {
-                five++;  
-            } else if (bill == 10) {
+            if (bill == 5) { five++; }
+             else if (bill == 10) {
                 if (five > 0) {
-                    five--;  
-                    ten++;   
-                } else {
-                    return false;  
-                }
-            } else if (bill == 20) {
+                    five--;
+                    ten++;
+                } else {return false; }
+            } else {
                 if (ten > 0 && five > 0) {
-                    ten--;  
-                    five--; 
+                    ten--;
+                    five--;
                 } else if (five >= 3) {
-                    five -= 3;  
-                } else {
-                    return false;  
-                }
+                    five -= 3;
+                } else { return false; }
             }
         }
-        
-        return true;  
+        return true;
     }
 };
