d282df096495ad1ab9e98902536b29fa9990ba66
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2025-02-03 07:33:52 +0530

    Time: 3 ms (8.24%) | Memory: 27.9 MB (64.71%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -4,18 +4,12 @@ public:
         int ans = 1;
         for (int i = 1, t = 1; i<nums.size(); ++i) {
             if (nums[i - 1] < nums[i]) {
-                ans = max(ans, ++t);
-            } else {
-                t = 1;
-            }
-        }
+                ans = max(ans, ++t);} 
+                else { t = 1; } }
         for (int i=1, t=1; i<nums.size(); ++i) {
             if (nums[i - 1] > nums[i]) {
-                ans = max(ans, ++t);
-            } else {
-                t = 1;
-            }
-        }
+                ans = max(ans, ++t); } 
+                else { t = 1; }}
         return ans;
     }
 };
