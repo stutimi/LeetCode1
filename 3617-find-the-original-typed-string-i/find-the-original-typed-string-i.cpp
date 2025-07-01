ac595e87d8ace3d8a7e6bf4acfc7be5c3eccd3ec
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2025-07-01 11:54:46 +0530

    Time: 5 ms (27.17%) | Memory: 9 MB (29.69%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count = 1;
+        for (int i = 1; i < word.length(); i++) {
+            if (word[i] == word[i - 1]) {
+                count++; 
+            }
+        }
+        return count; 
+    }
+};
